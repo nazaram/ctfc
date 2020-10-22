92d66273e121f8426fbe90ef84256d9ff9660bf7
obitobi13 <72237336+obitobi13@users.noreply.github.com>
2020-10-22 11:54:35 -0700

    minor stinky changes

--- game/dota_addons/ctfd/scripts/vscripts/heroes/hero_pudge/ctf_pudge_lil_stinky.lua
+++ game/dota_addons/ctfd/scripts/vscripts/heroes/hero_pudge/ctf_pudge_lil_stinky.lua
@@ -1,31 +1,34 @@
 LinkLuaModifier( "modifier_lil_stinky_slow", "heroes/hero_pudge/ctf_pudge_lil_stinky.lua", LUA_MODIFIER_MOTION_NONE )
 
-ctf_pudge_lil_stinky = class({})
-
 local gslow = 0
+
+ctf_pudge_lil_stinky = class({})
 
 function SlowHeroes ( keys )
 	local caster 				= keys.caster
 	local caster_location		= caster:GetAbsOrigin()
 	local ability 				= keys.ability
-	ability.level 				= ability:GetLevel()
-	local radius 				= ability:GetLevelSpecialValueFor("radius", ability.level - 1)
-	local slow 					= ability:GetLevelSpecialValueFor("movespeed_slow", ability.level - 1)
-	local plant_range			= ability:GetLevelSpecialValueFor("plant_range", ability.level - 1)
-	gslow 						= slow
-	local duration 				= ability:GetLevelSpecialValueFor("duration", ability.level - 1)
-	local particle_name1 		= "particles/units/heroes/hero_pudge/pudge_rot.vpcf"
-	
 
+	ability.level 				= ability:GetLevel()
+	gslow 						= ability:GetLevelSpecialValueFor("movespeed_slow", ability.level - 1)
+
+	local radius 				= ability:GetLevelSpecialValueFor("radius", ability.level - 1)
+	local plant_range			= ability:GetLevelSpecialValueFor("plant_range", ability.level - 1)
+	local duration 				= ability:GetLevelSpecialValueFor("duration", ability.level - 1)
+	local linger_duration		= ability:GetLevelSpecialValueFor("linger_duration", ability.level - 1)
+	local particle_name1 		= "particles/units/heroes/hero_pudge/pudge_rot.vpcf"
 	local target 				= - plant_range * caster:GetForwardVector()
 	local target_location		= caster_location + target
 	
 	local dummy = CreateUnitByName("npc_dota_custom_dummy_unit", target_location, false, caster, caster, caster:GetTeamNumber())
-
 	local particle1 = ParticleManager:CreateParticle(particle_name1, PATTACH_ABSORIGIN, dummy)
 
 	ParticleManager:SetParticleControl(particle1, 0, dummy:GetAbsOrigin())
 	ParticleManager:SetParticleControl(particle1, 1, Vector(radius, 0, 0))
+
+
+
+	-- dummy:OnIntervalThink()
 
 	local units = FindUnitsInRadius(
 		caster:GetTeam(), 
@@ -41,7 +44,7 @@ function SlowHeroes ( keys )
 	for _, unit in ipairs(units) do
 
 		if ((unit:GetAbsOrigin() - dummy:GetAbsOrigin()):Length2D()) < radius then
-			unit:AddNewModifier(caster, ability, "modifier_lil_stinky_slow", {duration = duration})
+			unit:AddNewModifier(caster, ability, "modifier_lil_stinky_slow", {duration = linger_duration})
 		else
 			unit:RemoveModifierByName("modifier_lil_stinky_slow")
 		end
@@ -57,9 +60,20 @@ function SlowHeroes ( keys )
 		)
 end
 
+lil_cloud_thinker = class({})
+
+function lil_cloud_thinker:OnCreated(event)
+	local thinker = self:GetParent()
+	local ability = self:GetAbilty()
+
+	print("Thinker = ", thinker)
+end
+
+
+
 modifier_lil_stinky_slow = class({})
 
-function modifier_lil_stinky_slow:isHidden()
+function modifier_lil_stinky_slow:IsHidden()
 	return false
 end
 
@@ -72,11 +86,11 @@ function modifier_lil_stinky_slow:IsPurgable()
 end
 
 function modifier_lil_stinky_slow:GetEffectName()
-	return "particles/units/heroes/hero_pudge/pudge_rot_body_spores.vpcf"
+	return "particles/econ/items/pudge/pudge_tassles_of_black_death/pudge_rot_recipient_d_dc.vpcf"
 end
 
 function modifier_lil_stinky_slow:GetEffectAttachType()
-	return PATTACH_ABSORIGIN
+	return PATTACH_ABSORIGIN_FOLLOW
 end
 
 function modifier_lil_stinky_slow:GetTexture()
