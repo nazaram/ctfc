1984419c3573ecc3584a2b06b09c766b8be1d80d
obitobi13 <72237336+obitobi13@users.noreply.github.com>
2020-10-24 14:33:25 -0700

    Void Spirit Done

--- game/dota_addons/ctfd/scripts/vscripts/heroes/hero_void_spirit/ctf_void_spirit_astral_step.lua
+++ game/dota_addons/ctfd/scripts/vscripts/heroes/hero_void_spirit/ctf_void_spirit_astral_step.lua
@@ -1,3 +1,7 @@
+LinkLuaModifier( "modifier_void_mark", "heroes/hero_void_spirit/ctf_void_spirit_astral_step.lua", LUA_MODIFIER_MOTION_NONE )
+
+gslow = -80
+
 ctf_void_spirit_astral_step = class({})
 
 function OnSpellStart( keys )
@@ -6,7 +10,7 @@ function OnSpellStart( keys )
 	local particle_name 	= "particles/units/heroes/hero_void_spirit/astral_step/void_spirit_astral_step.vpcf"
 
 	caster_location = caster:GetAbsOrigin()
-	ability_level = ability:GetLevel() - 1
+	ability_level = ability:GetLevel()
 
 	local radius 				= ability:GetLevelSpecialValueFor("radius", ability_level - 1)
 	local min_travel_distance 	= ability:GetLevelSpecialValueFor("min_travel_distance", ability_level - 1)
@@ -29,4 +33,49 @@ function OnSpellStart( keys )
 	ParticleManager:SetParticleControl(particle, 0, caster_location)
 	ParticleManager:SetParticleControl(particle, 1, target_point)
 
+	local units = FindUnitsInLine(caster:GetTeamNumber(), caster_location, target_point, caster, radius, DOTA_UNIT_TARGET_TEAM_ENEMY, DOTA_UNIT_TARGET_HERO, DOTA_UNIT_TARGET_FLAG_NONE)
+
+
+	for _, unit in ipairs(units) do
+		unit:AddNewModifier(caster, ability, "modifier_void_mark", {duration = slow_duration})					
+	end
+end
+
+modifier_void_mark = class({})
+
+function modifier_void_mark:IsHidden()
+	return false
+end
+
+function modifier_void_mark:IsDebuff()
+	return true
+end
+
+function modifier_void_mark:IsPurgable()
+	return true
+end
+
+function modifier_void_mark:GetEffectName()
+	return "particles/units/heroes/hero_void_spirit/astral_step/void_spirit_astral_step_debuff.vpcf"
+end
+
+function modifier_void_mark:GetEffectAttachType()
+	return PATTACH_ABSORIGIN_FOLLOW
+end
+
+function modifier_void_mark:GetTexture()
+	return "void_spirit_astral_step"
+end
+
+function modifier_void_mark:DeclareFunctions()
+	local funcs = {
+
+		MODIFIER_PROPERTY_MOVESPEED_BONUS_PERCENTAGE
+	}
+
+	return funcs
+end
+
+function modifier_void_mark:GetModifierMoveSpeedBonus_Percentage()
+	return gslow
 end
